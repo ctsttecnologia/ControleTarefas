d98732c1b161663672084ddd255bcc2ddcbaa681
ctsttecnologia <ctsttecnologia@gmail.com>
2025-11-09 18:24:32 -0300

    App Chat
    Correções

--- chat/urls.py
+++ chat/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'chat'
 
 urlpatterns = [
-    # Dashboard principal do chat
-    
+       
     # APIs
     path('api/users/', views.get_user_list, name='get_user_list'),
     path('api/tasks/', views.get_task_list, name='get_task_list'),
@@ -13,4 +12,5 @@ urlpatterns = [
     path('api/group/create/', views.create_group_chat, name='create_group'),
     path('api/task/<int:task_id>/', views.get_or_create_task_chat, name='get_task_chat'),
     path('api/history/<uuid:room_id>/', views.get_chat_history, name='get_history'),
+    path('api/upload/', views.ChatImageUploadView.as_view(), name='upload_image'),
 ]
