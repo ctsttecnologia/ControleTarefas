8cf0aa848847e341994009418412c4eb2df788fc
deploy <deploy@container>
2025-11-28 14:00:47 -0300

    Minhas mudanças locais no asgi.py antes do pull

--- gerenciandoTarefas/asgi.py
+++ gerenciandoTarefas/asgi.py
@@ -1,24 +1,29 @@
-# gerenciandoTarefas/asgi.py
-
 import os
-import django
 from django.core.asgi import get_asgi_application
 
+# 1. Configura o settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciandoTarefas.settings')
-django.setup()  # IMPORTANTE: Garante que Django está configurado
 
-# Agora importe o resto
+# 2. Carrega o Django HTTP (Crucial fazer isso ANTES de importar o channels)
+django_asgi_app = get_asgi_application()
+
+# 3. Importações do Channels (Só agora!)
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 import chat.routing
 
+# 4. Define o Roteador
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
+    # http -> Django nativo
+    "http": django_asgi_app,
+
+    # websocket -> Django Channels
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
         )
     ),
 })
-
-
