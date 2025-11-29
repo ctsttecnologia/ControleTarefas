42f5664e48ba15a2a8fa6b8162ec3136212bc88b
ctsttecnologia <ctsttecnologia@gmail.com>
2025-11-29 18:44:36 -0300

    App Chat
    Correção

--- chat/routing.py
+++ chat/routing.py
@@ -4,9 +4,14 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    # Rota para o chat DENTRO de uma sala
+    # Aceita tanto 'ws/chat/...' quanto 'wss/chat/...' para garantir
     re_path(r'ws/chat/(?P<room_id>[0-9a-f-]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<room_id>[0-9a-f-]+)/$', consumers.ChatConsumer.as_asgi()),
     
+    # Rota de notificações
+    re_path(r'wss/notifications/$', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
+
     # Rota para o socket de Notificações GERAIS
     path('ws/notifications/', consumers.NotificationConsumer.as_asgi()),
 ]
