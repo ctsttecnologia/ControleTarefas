cd38fabb5c735fc60b32b23e8e787dad9d77cd88
ctsttecnologia <ctsttecnologia@gmail.com>
2026-04-10 09:15:49 -0300

    Correção de rota de notificação

--- notifications/urls.py
+++ notifications/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'notifications'
 
 urlpatterns = [
-    path('', views.notificacao_list, name='lista'),
+    path('', views.notificacao_list, name='notificacao_list'),
     path('<int:pk>/lida/', views.marcar_como_lida, name='marcar_como_lida'),
     path('marcar-todas/', views.marcar_todas_como_lidas, name='marcar_todas_como_lidas'),
     path('api/contagem/', views.api_contagem, name='api_contagem'),
